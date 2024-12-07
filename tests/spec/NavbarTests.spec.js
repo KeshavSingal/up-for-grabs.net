0aa45b0edc2ab2147ef836b38ada966ce0cb8d5d
Keshav Jindal <keshav.jindal@mail.utoronto.ca>
2024-12-06 22:29:15 -0500

    Ran prettier

--- tests/spec/NavbarTests.spec.js
+++ tests/spec/NavbarTests.spec.js
@@ -1,12 +1,13 @@
 var assert = require('assert'),
-    jsdom = require('jsdom');
+  jsdom = require('jsdom');
 
-describe('Navbar', function() {
+describe('Navbar', function () {
   var window, document;
-  
-  beforeEach(function() {
+
+  beforeEach(function () {
     // Set up a simple DOM environment
-    const dom = new jsdom.JSDOM(`
+    const dom = new jsdom.JSDOM(
+      `
       <nav class="nav-menu">
         <ul>
           <li><a href="#i-want-to-get-involved">Get Involved</a></li>
@@ -17,10 +18,12 @@ describe('Navbar', function() {
       <div id="i-want-to-get-involved">Section 1</div>
       <div id="projects">Section 2</div>
       <div id="i-maintain-a-project">Section 3</div>
-    `, {
-      url: "http://localhost",
-      runScripts: "dangerously"
-    });
+    `,
+      {
+        url: 'http://localhost',
+        runScripts: 'dangerously',
+      }
+    );
 
     window = dom.window;
     document = window.document;
@@ -28,23 +31,23 @@ describe('Navbar', function() {
     // Simple mock for scrollTo
     window.scrollTo = jest.fn();
     window.performance = { now: () => Date.now() };
-    window.requestAnimationFrame = callback => setTimeout(callback, 0);
+    window.requestAnimationFrame = (callback) => setTimeout(callback, 0);
   });
 
   // Test 1: Basic structure
-  it('should have navigation menu', function() {
+  it('should have navigation menu', function () {
     var nav = document.querySelector('.nav-menu');
     assert.notEqual(nav, null);
   });
 
   // Test 2: Number of links
-  it('should have three navigation links', function() {
+  it('should have three navigation links', function () {
     var links = document.querySelectorAll('.nav-menu a');
     assert.equal(links.length, 3);
   });
 
   // Test 3: Link hrefs
-  it('should have correct href attributes', function() {
+  it('should have correct href attributes', function () {
     var links = document.querySelectorAll('.nav-menu a');
     assert.equal(links[0].getAttribute('href'), '#i-want-to-get-involved');
     assert.equal(links[1].getAttribute('href'), '#projects');
@@ -52,7 +55,7 @@ describe('Navbar', function() {
   });
 
   // Test 4: Link text
-  it('should have correct link text', function() {
+  it('should have correct link text', function () {
     var links = document.querySelectorAll('.nav-menu a');
     assert.equal(links[0].textContent, 'Get Involved');
     assert.equal(links[1].textContent, 'Projects');
@@ -60,12 +63,14 @@ describe('Navbar', function() {
   });
 
   // Test 5: Basic scroll check
-  it('should call scrollTo when link is clicked', function(done) {
+  it('should call scrollTo when link is clicked', function (done) {
     // Setup click handler
-    document.querySelector('.nav-menu a').addEventListener('click', function(e) {
-      e.preventDefault();
-      window.scrollTo(0, 100);
-    });
+    document
+      .querySelector('.nav-menu a')
+      .addEventListener('click', function (e) {
+        e.preventDefault();
+        window.scrollTo(0, 100);
+      });
 
     // Click the link
     document.querySelector('.nav-menu a').click();
